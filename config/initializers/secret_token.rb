722981e0b68addcd0492f2571fe95eb428c7a642
fukuishere <fukuishere@gmail.com>
2013-05-11 15:55:43 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dokoiku::Application.config.secret_token = '79989cefbec0217c690aadc4b68f0f13cae53f15853cf2cad7295b539fab5829c1ff333ffa3f1f4280b13295fc65934b762d0ba9bc5de4fc42abc134b7081224'
